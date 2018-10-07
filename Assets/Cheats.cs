8439ac1ffb687767b21d8120cfaf38af31dca73a
Paul1005 <mccarliep@gmail.com>
2018-10-06 21:52:36 -0700

    more stuff
    character can now move and turn collision on and off with Y key

---
+++ Assets/Cheats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : MonoBehaviour
+{
+    GameObject[] walls;
+    bool isActive;
+    // Use this for initialization
+    void Start()
+    {
+        isActive = true;
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Y)/* || Input.GetAxis(ZAxis) > 0*/)
+        {
+            isActive = !isActive;
+            Debug.Log(isActive);
+            foreach(GameObject wall in walls){
+                wall.GetComponent<MeshCollider>().enabled = isActive;
+            }
+        }
+    }
+}
