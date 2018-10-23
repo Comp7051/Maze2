59ae8122f8f0082daef77287ef8c08fcd1438947
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-10-23 10:18:04 -0700

    Really early version of ambient light via shader + script.

---
+++ Assets/Scripts/RenderController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderController : MonoBehaviour {
+
+	Renderer rend;
+	float originalAmbientIntensity;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		//originalAmbientIntensity = rend.material.GetFloat ("Ambient Light Intensity");
+		originalAmbientIntensity = Shader.GetGlobalFloat ("_AmbientLighIntensity");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.N)) {
+			// Switch to Night mode.
+			originalAmbientIntensity+=0.1f;
+			Shader.SetGlobalFloat ("_AmbientLighIntensity", originalAmbientIntensity);
+//			rend.material.SetFloat ("_AmbientLighIntensity", originalAmbientIntensity);
+			Debug.Log(Shader.GetGlobalFloat ("_AmbientLighIntensity"));
+		}
+
+	}
+}
