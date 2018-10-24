7b5c08573a2ed67545603a162cd8a84d950e70f6
Paul1005 <mccarliep@gmail.com>
2018-10-23 22:39:37 -0700

    more stuff

--- Assets/GenerateMaze.cs
+++ Assets/GenerateMaze.cs
@@ -10,28 +10,48 @@ public class GenerateMaze : MonoBehaviour
     private int[,] maze;
     private int x;
     private int y;
-    private int[] list;
+    private List<GameObject> list;
+    private List<List<GameObject>> cells;
 
     // Use this for initialization
     void Start()
     {
-        x = Random.Range(0, width);
-        y = Random.Range(0, height);
-
-        maze = new int[width, height];
 
         GenerateCells();
+
+        CreateMaze();
+    }
+
+    private void CreateMaze()
+    {
+        int[] validChoices = { 1, -1 };
+
+        x = Random.Range(0, width);
+        y = Random.Range(0, height);
+        list.Add(cells[x][y]);
+
+        while (list.Count > 0)
+        {
+            int nextCell = validChoices[Random.Range(0, 1)];
+            list.Add(cells[x][y]);
+
+
+        }
     }
 
     private void GenerateCells()
     {
+        cells = new List<List<GameObject>>();
         for (int i = 0; i < width; i++)
         {
+            List<GameObject> row = new List<GameObject>();
             for (int j = 0; j < width; j++)
             {
                 GameObject cell = Instantiate(Resources.Load("Cell")) as GameObject;
                 cell.transform.position = new Vector3(5 + 10 * i, 0, 5 + 10 * j);
+                row.Add(cell);
             }
+            cells.Add(row);
         }
     }
 }
