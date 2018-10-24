0bb14385f245274fc235ff7c82f6b8d6f3c8e7b0
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-10-23 23:39:33 -0700

    Get an ambient shader working on the floor... Toggle day/night with N.

---
+++ Assets/Scripts/RenderManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderManager : MonoBehaviour {
+
+	private float ambientIntensity;
+
+	// Use this for initialization
+	void Start () {
+		ambientIntensity = 0.0f;
+		Shader.SetGlobalFloat ("_Ambient", ambientIntensity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.N)) {
+			if (ambientIntensity >= 1.0f)
+				ambientIntensity = 0.0f;
+			else if (ambientIntensity <= 0.0f)
+				ambientIntensity = 1.0f;
+			
+			Shader.SetGlobalFloat ("_Ambient", ambientIntensity);
+			Debug.Log (Shader.GetGlobalFloat ("_Ambient"));
+		}
+	}
+}
