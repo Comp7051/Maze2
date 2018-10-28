2fa171970e41654db2788014d1301c7351eeb732
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-10-28 04:38:59 -0700

    Set day and night values for ambient lighting.

--- Assets/Scripts/RenderManager.cs
+++ Assets/Scripts/RenderManager.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class RenderManager : MonoBehaviour {
 
 	private float ambientIntensity;
+	public float ambientIntensityDay;
+	public float ambientIntensityNight;
 
 	// Use this for initialization
 	void Start () {
-		ambientIntensity = 0.0f;
+		ambientIntensity = ambientIntensityDay;
 		Shader.SetGlobalFloat ("_Ambient", ambientIntensity);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp (KeyCode.N)) {
-			if (ambientIntensity >= 1.0f)
-				ambientIntensity = 0.0f;
-			else if (ambientIntensity <= 0.0f)
-				ambientIntensity = 1.0f;
+			if (ambientIntensity >= ambientIntensityDay)
+				ambientIntensity = ambientIntensityNight;
+			else if (ambientIntensity <= ambientIntensityNight)
+				ambientIntensity = ambientIntensityDay;
 
 			Shader.SetGlobalFloat ("_Ambient", ambientIntensity);
 			Debug.Log (Shader.GetGlobalFloat ("_Ambient"));
