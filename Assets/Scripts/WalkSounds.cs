ec4c5e1d1e9618a02699f296a6116d541e113aa4
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-11-07 05:36:41 -0800

    Get enemy using animation events to trigger walk sounds as well.

--- Assets/Scripts/WalkSounds.cs
+++ Assets/Scripts/WalkSounds.cs
@@ -35,32 +35,34 @@ public class WalkSounds : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		float speed = rb.velocity.magnitude;
-		float delay = 0.25f;
+		if (this.CompareTag ("Player")) {
+			float speed = rb.velocity.magnitude;
+			float delay = 0.25f;
 
-		if (speed > 2.0f) {
-			if (speed > 30.0f) {
-				delay *= 0.7f;
-			} else if (speed > 10.0f) {
-				delay /= 0.8f;
-			} else if (speed > 4.0f) {
-				delay /= 0.9f;
-			} else if (speed > 3.0f) {
-				delay /= 1.0f;
-			} else if (speed > 2.0f && speed <= 3.0f) {
-				delay *= 1.2f;
-			}
+			if (speed > 2.0f) {
+				if (speed > 30.0f) {
+					delay *= 0.7f;
+				} else if (speed > 10.0f) {
+					delay /= 0.8f;
+				} else if (speed > 4.0f) {
+					delay /= 0.9f;
+				} else if (speed > 3.0f) {
+					delay /= 1.0f;
+				} else if (speed > 2.0f && speed <= 3.0f) {
+					delay *= 1.2f;
+				}
 
-			if (Time.fixedTime > timeOfLastStep + delay) {
-				timeOfLastStep = Time.fixedTime;
+				if (Time.fixedTime > timeOfLastStep + delay) {
+					timeOfLastStep = Time.fixedTime;
 
-				footIndicator = footIndicator == (int)Foot.LEFT ? (int)Foot.RIGHT : (int)Foot.LEFT;
-				FootStep (footIndicator);
+					footIndicator = footIndicator == (int)Foot.LEFT ? (int)Foot.RIGHT : (int)Foot.LEFT;
+					FootStep (footIndicator);
+				}
 			}
 		}
 	}
 
-	void FootStep(int footIndicator) {
+	public void FootStep(int footIndicator) {
 		int soundToPlay;
 		// Just avoid playing the same sound twice in a row.
 		do {
