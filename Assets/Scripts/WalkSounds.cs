d93fbcc99c92ea5957e73e3abfaa64399e405f1b
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-11-07 03:54:13 -0800

    Velocity-aware footsteps.

--- Assets/Scripts/WalkSounds.cs
+++ Assets/Scripts/WalkSounds.cs
@@ -6,15 +6,13 @@ using System;
 public class WalkSounds : MonoBehaviour {
 
 	public List<AudioClip> walkSounds = new List<AudioClip>();
-	public float secondsBetweenSteps;
 
 	private enum Foot {
 		LEFT = 0,
 		RIGHT
 	};
 
-	private Vector3 lastPos;
-	private float timeOfNextStep;
+	private float timeOfLastStep;
 	private AudioSource[] foot;
 	private int footIndicator;
 	public AudioSource leftFoot;
@@ -22,32 +20,55 @@ public class WalkSounds : MonoBehaviour {
 
 	private int lastSoundIndex;
 
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		lastPos = this.transform.position;
-		timeOfNextStep = 0.0f;
+		timeOfLastStep = 0.0f;
 		foot = new AudioSource[2];
 		foot [(int)Foot.LEFT] = leftFoot;
 		foot [(int)Foot.RIGHT] = rightFoot;
 		footIndicator = 0;
 		lastSoundIndex = -1;
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (timeOfNextStep < Time.fixedTime) {
-			int soundToPlay;
-			// Just avoid playing the same sound twice in a row.
-			do {
-				System.Random rnd = new System.Random ();
-				soundToPlay = rnd.Next (walkSounds.Count);
-			} while (soundToPlay == lastSoundIndex);
-			lastSoundIndex = soundToPlay;
+		float speed = rb.velocity.magnitude;
+		float delay = 0.25f;
 
-			footIndicator = footIndicator == (int)Foot.LEFT ? (int)Foot.RIGHT : (int)Foot.LEFT;
-			timeOfNextStep = Time.fixedTime + secondsBetweenSteps;
+		if (speed > 2.0f) {
+			if (speed > 30.0f) {
+				delay *= 0.7f;
+			} else if (speed > 10.0f) {
+				delay /= 0.8f;
+			} else if (speed > 4.0f) {
+				delay /= 0.9f;
+			} else if (speed > 3.0f) {
+				delay /= 1.0f;
+			} else if (speed > 2.0f && speed <= 3.0f) {
+				delay *= 1.2f;
+			}
 
-			foot [footIndicator].PlayOneShot (walkSounds [soundToPlay]);
+			if (Time.fixedTime > timeOfLastStep + delay) {
+				timeOfLastStep = Time.fixedTime;
+
+				footIndicator = footIndicator == (int)Foot.LEFT ? (int)Foot.RIGHT : (int)Foot.LEFT;
+				FootStep (footIndicator);
+			}
 		}
+	}
+
+	void FootStep(int footIndicator) {
+		int soundToPlay;
+		// Just avoid playing the same sound twice in a row.
+		do {
+			System.Random rnd = new System.Random ();
+			soundToPlay = rnd.Next (walkSounds.Count);
+		} while (soundToPlay == lastSoundIndex);
+		lastSoundIndex = soundToPlay;
+
+		foot [footIndicator].PlayOneShot (walkSounds [soundToPlay]);
 	}
 }
