8ee6aad1b0c9061b07480dde23bdd804cc6cceed
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-11-11 20:00:51 -0800

    Add a soccer ball asset from the store, get some scripting in to throw it.

---
+++ Assets/Scripts/ThrowBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject clone;
+			clone = Instantiate (ball, Camera.main.transform.position, Camera.main.transform.rotation);//, transform.forward);
+			clone.GetComponent<Rigidbody>().velocity = clone.transform.forward * 10;
+
+		}
+	}
+}
