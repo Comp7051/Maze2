ea94fab5de274f9f4e4a49fa6894a65c8d409b94
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2018-11-27 03:46:31 -0800

    Modulate music based upon normalized(to the maze's total dimensions) distance between player and enemy.

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -20,10 +20,24 @@ public class MusicManager : MonoBehaviour {
 	private float dayVolumeNormal;
 	private float nightVolumeNormal;
 
+	private GameObject player;
+	private GameObject enemy;
+
+	private float maxDistance;
+
 	// Use this for initialization
 	void Start () {
 		dayVolumeNormal = dayMusic.volume;
 		nightVolumeNormal = nightMusic.volume;
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+		enemy = GameObject.FindGameObjectWithTag ("Enemy");
+
+		GenerateMaze mazeScript = GameObject.FindGameObjectWithTag ("Maze").GetComponent<GenerateMaze> ();
+		Bounds cellBounds = GameObject.FindGameObjectWithTag ("Cell").GetComponent<Renderer>().bounds;
+
+		Vector3 mazeBounds = new Vector3 (cellBounds.size.x * mazeScript.width, cellBounds.size.y * mazeScript.width, cellBounds.size.z * mazeScript.width);
+		maxDistance = Mathf.Sqrt (Mathf.Pow (mazeBounds.x, 2) + Mathf.Pow (mazeBounds.z, 2)); // Hypotenuse of the maze's total dimensions
 	}
 	
 	// Update is called once per frame
@@ -36,6 +50,15 @@ public class MusicManager : MonoBehaviour {
 		if (updateNeeded) {
 			UpdateMusic ();
 			updateNeeded = false;
+		}
+
+		float musicModulation = (1.0f - Vector3.Distance (player.transform.position, enemy.transform.position) / maxDistance);
+		if (fogState) {
+			dayMusic.volume = (dayVolumeNormal /2) * musicModulation;
+			nightMusic.volume = (nightVolumeNormal /2) * musicModulation;
+		} else {
+			dayMusic.volume = dayVolumeNormal * musicModulation;
+			nightMusic.volume = nightVolumeNormal * musicModulation;
 		}
 	}
 
@@ -65,13 +88,13 @@ public class MusicManager : MonoBehaviour {
 			}
 		}
 
-		if (fogState) {
+		/*if (fogState) {
 			dayMusic.volume = dayVolumeNormal / 2;
 			nightMusic.volume = nightVolumeNormal / 2;
 		} else {
 			dayMusic.volume = dayVolumeNormal;
 			nightMusic.volume = nightVolumeNormal;
-		}
+		}*/
 	}
 
 	public static void FogState(bool fogState) {
